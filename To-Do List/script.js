35f501897ed15d9577d9dbe863d1b18bf93f5883
AnimeshRaj <animesh080808@gmail.com>
2022-08-25 16:15:06 +0530

    Created Files For To Do List

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -1,6 +1,6 @@
-const form = document.querySelector("form");
-const input = document.querySelector('input');
-const list = document.querySelector("ul");
+const form = document.querySelector("form")
+const input = document.querySelector('input')
+const list = document.querySelector("ul")
 form.addEventListener("submit" , function(e){
     e.preventDefault();
     toDo();
@@ -14,17 +14,28 @@ function toDo(){
         const value = input.value;
         /* Creating li element each time we submit */
         const newList = document.createElement('Li');
+        newList.classList.add("taskList")
         /* Adding the value of the input to the li*/
-        newList.textContent = value;
+
+        newList.textContent =  value ;
         /* Adding the delete button*/
-        const deleteBtn = document.createElement("button");
+        const deleteBtn = document.createElement("button")
+        deleteBtn. className = "deleteButton";
         /* Changing the content of the button*/
         deleteBtn.textContent = "Delete";
         /* Adding the li inside the ul  */
         list.append(newList);
         /* Adding the delete button */
-        new
+        newList.append(' ', deleteBtn)
+
 
     }
     
 }
+
+// For deleting:
+list.addEventListener('click', function(e){
+    if (e.target.nodeName === 'BUTTON'){
+    e.target.closest('LI').remove();
+    }
+})
